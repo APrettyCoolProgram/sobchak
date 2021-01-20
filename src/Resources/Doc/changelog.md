ccfc37e2ae78fb0b0f4c8564575774e9f800eb47
Developer <development@aprettycoolprogram.com>
2021-01-20 09:13:14 -0500

    v0.1.21020.1411
    * `ADDED` du.proj
    * `UPDATED` Documentation
    * `UPDATED` sobchak.licenseheader

--- src/Resources/Doc/changelog.md
+++ src/Resources/Doc/changelog.md
@@ -1,4 +1,15 @@
 ﻿# Sobchak: Changelog
 
-#### v0.0
-`INFO` Initial commit
+#### 0.1
+> Project framework updates
+
+#### v0.1.21020.1358 (2021-01-20)
+* `ADDED` du.proj
+* `UPDATED` Documentation
+* `UPDATED` sobchak.licenseheader
+
+## Version 0.0 - January 19, 2021
+> Initial commit, no working code.
+
+#### v0.0.0.0 (2021-01-19)
+* `ADDED` Initial project framework
---
+++ src/SobchakMain.xaml.cs
@@ -0,0 +1,22 @@
+﻿/* PROJECT: Sobchak (https://github.com/aprettycoolprogram/Sobchak)
+ *    FILE: Sobchak.SobchakMain.xaml.cs
+ * UPDATED: 1-20-2021-8:57 AM
+ * LICENSE: Apache v2 (https://apache.org/licenses/LICENSE-2.0)
+ *          Copyright 2020 A Pretty Cool Program All rights reserved
+ */
+
+using System.Windows;
+
+namespace Sobchak
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
